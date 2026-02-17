572be087c240cf3e49294ad3520657153308756c
Hamza Siddique <6793siddique@gmail.com>
2026-02-17 23:08:34 +0500

    update password

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -40,41 +40,12 @@ class ProfileController extends Controller
 
         $user = Auth::user();
 
-        $user->update(['name' => $request->name]);
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email
+        ]);
 
         Session::flash('account', __('Account information successfully updated.'));
         return back();
-    }
-
-    public function update_paxful_api(Request $request, User $user)
-    {
-        $user->metas()->updateOrCreate(
-            ['meta_key' => 'paxful_api_key'],
-            ['meta_value' => $request->api_key]);
-
-        $user->metas()->updateOrCreate(
-            ['meta_key' => 'paxful_api_secret'],
-            ['meta_value' => $request->api_secret]);
-
-        Session::flash('account', 'API Key updated successfully.');
-        return redirect()->back();
-    }
-
-    public function update_availability_status(Request $request)
-    {
-        $user = Auth::user();
-        $status = 0;
-
-        if ($request->has('status')) {
-            $status = 1;
-        }
-
-        $user->metas()->updateOrCreate(
-            ['meta_key' => 'availability'],
-            ['meta_value' => $status]);
-
-
-        Session::flash('account', 'Status updated successfully.');
-        return redirect()->back();
     }
 }
